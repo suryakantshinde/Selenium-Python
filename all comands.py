31399814f5b61a24be947d6f88e6fec0f71b81c2
Suryakant Shinde <52278674+suryakantshinde@users.noreply.github.com>
2025-01-17 12:46:26 +0530

    Create all comands.py

---
+++ all comands.py
@@ -0,0 +1,88 @@
+pip --trusted-host=pypi.python.org --trusted-host=pypi.org --trusted-host=files.pythonhosted.org install selenium
+pip --trusted-host=pypi.python.org --trusted-host=pypi.org --trusted-host=files.pythonhosted.org m pip install --upgrade pip
+
+pip --trusted-host=pypi.python.org --trusted-host=pypi.org --trusted-host=files.python.exe -m pip install --upgrade pip
+ 
+
+
+"C:/Program Files/Python38/python.exe" -m pip  --trusted-host=pypi.python.org --trusted-host=pypi.org --trusted-host=files.pythonhosted.org install -U pylint
+
+pip --trusted-host=pypi.python.org --trusted-host=pypi.org --trusted-host=files.pythonhosted.org install selenium
+
+pip --trusted-host=pypi.python.org --trusted-host=pypi.org --trusted-host=files.pythonhosted.org install -U pip
+
+pip --trusted-host=pypi.python.org --trusted-host=pypi.org --trusted-host=files.pythonhosted.or --upgrade pip
+
+ --trusted-host=pypi.python.org --trusted-host=pypi.org --trusted-host=files.pythonhosted.org 
+
+--trusted-host=pypi.python.org --trusted-host=pypi.org --trusted-host=files.pythonhosted.org --upgrade pip
+
+pip --trusted-host=pypi.python.org --trusted-host=pypi.org --trusted-host=files.pythonhosted.org install -U pip.....upgrade Pip
+
+pip --trusted-host=pypi.python.org --trusted-host=pypi.org --trusted-host=files.pythonhosted.org install -linter
+
+pip --trusted-host=pypi.python.org --trusted-host=pypi.org --trusted-host=files.pythonhosted.org install -Pandas
+
+-m pip --trusted-host=pypi.python.org --trusted-host=pypi.org --trusted-host=files.pythonhosted.org install -u pytest --User
+
+-m pip --trusted-host=pypi.python.org --trusted-host=pypi.org --trusted-host=files.pythonhosted.org install pyinstaller
+pip --trusted-host=pypi.python.org --trusted-host=pypi.org --trusted-host=files.pythonhosted.org install pyinstaller......................................worked
+from Screenshot import Screenshot_clipping
+pip --trusted-host=pypi.python.org --trusted-host=pypi.org --trusted-host=files.pythonhosted.org install Screenshot
+
+
+
+Z:\>pip --trusted-host=pypi.python.org --trusted-host=pypi.org --trusted-host=files.pythonhosted.org install xlrd ..................worked install excel liberies
+
+
+
+
+pip --trusted-host=pypi.python.org --trusted-host=pypi.org --trusted-host=files pip install poetry
+
+pip --trusted-host=pypi.python.org --trusted-host=pypi.org --trusted-host=files.pythonhosted.org install --upgrade pip
+
+
+
+Using cached pip-23.2.1-py3-none-any.whl (2.1 MB)
+ERROR: To modify pip, please run the following command:
+C:\Program Files\Python311\python.exe -m pip --trusted-host=pypi.python.org --trusted-host=pypi.org --trusted-host=files.pythonhosted.org install --upgrade pip
+
+
+
+
+"C:\Program Files\Python311\Scripts\pip3.11.exe -m pip --trusted-host=pypi.python.org --trusted-host=pypi.org --trusted-host=files.pythonhosted.org install --upgrade pip 
+
+
+C:\Program Files\Python311\Scripts\pip3.11.exe -m pip --trusted-host=pypi.python.org --trusted-host=pypi.org --trusted-host=files.pythonhosted.org install poetry
+
+
+pip install --upgrade pip
+
+
+
+
+
+browser = webdriver.Chrome(executable_path="C:\\PY_VCR_Accept_Without_Switch_Profile\\Chrome_Browse\\chromedriver.exe")
+#"C:\PY_VCR_Accept_Without_Switch_Profile\Chrome_Browse\chromedriver.exe"
+browser.maximize_window()
+browser.get('https://secure.visaonline.com//SitePages//GVOLHome.aspx')    
+#enter user name
+element = browser.find_element_by_id("txtUsername")
+element.send_keys("Vaishali.Sawant@firstdata.com")
+#enter Password
+element = browser.find_element_by_id("txtPd")
+element.send_keys("May@#2021")
+#Click on Login Button
+element=browser.find_element_by_id('btnLogin').click()
+# wait for Page to Display
+browser.implicitly_wait(5)
+# Second Page
+element=browser.find_element_by_id('ROL').click()
+
+
+
+<iframe id="buttons" name="buttons" scrolling="no" src="/rolol/jsp/buttons_index.jsp" class="width100 height100" frameborder="0"></iframe>
+https://www.youtube.com/watch?v=QImJmz1NmLY
+
+
+Selenium-VBA provides a wrapper to bind selenium 1 and Selenium 2 with VBA in excel. That means, you can automate webdrivers using excel macros
